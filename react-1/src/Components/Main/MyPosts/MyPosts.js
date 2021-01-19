aa089f9e2bc8089de6cc3ad2a5b61f43908de1a9
Andrii Hrohodza <50321565+AndriiHrohodza@users.noreply.github.com>
2021-01-19 17:16:59 +0200

    New components for main posts nav

---
+++ react-1/src/Components/Main/MyPosts/MyPosts.js
@@ -0,0 +1,13 @@
+import Post from "./Post/Post";
+
+const MyPosts = () => {
+  return (
+    <ul className="main-posts">
+      Posts
+      <Post/>
+      <Post/>
+    </ul>
+  )
+}
+
+export default MyPosts;
---
+++ react-1/src/Components/Main/Profile/Profile.js
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div className="main-profile">
+      Ava+name
+    </div>
+  )
+}
+
+export default Profile;
